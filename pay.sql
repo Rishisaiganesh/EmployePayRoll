3f18ae5532e2e25690cc72d2b4c7d8889a8fb435
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-02 11:15:28 +0530

    Retriving Data from Table

--- pay.sql
+++ pay.sql
@@ -10,3 +10,5 @@ StartingDate Date
 insert into EmpPayroll(EmployeeName, Salary, StartingDate)
 values('Rishi', 250000, convert (date,'2009-10-19'));
 select * from EmpPayroll;
+select Salary from EmpPayroll where EmployeeName='Rishi';
+select * from EmpPayroll where StartingDate between cast('2005-8-19' as date) and cast('2014-05-19' as date);
