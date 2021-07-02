babc6661100b2d8da63b7f38236ca94a6a89345c
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-02 11:22:31 +0530

    Updating Table

--- pay.sql
+++ pay.sql
@@ -12,3 +12,8 @@ values('Rishi', 250000, convert (date,'2009-10-19'));
 select * from EmpPayroll;
 select Salary from EmpPayroll where EmployeeName='Rishi';
 select * from EmpPayroll where StartingDate between cast('2005-8-19' as date) and cast('2014-05-19' as date);
+
+alter table EmpPayroll
+add Gender varchar(255);
+update EmpPayroll set Gender='Femail' where EmployeeName='rani' or EmployeeName = 'Madhu';
+update EmpPayroll set Gender='Male'where EmployeeName='Rishi';
