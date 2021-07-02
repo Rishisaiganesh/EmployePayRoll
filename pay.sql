47fa78570327a927cf8e5fc17e1ffcaa044bdf09
Rishisaiganesh <crazyganesh67@gmail.com>
2021-07-02 11:05:16 +0530

    Creating Table

--- pay.sql
+++ pay.sql
@@ -1,4 +1,9 @@
 
 create database PayingrollService;
 use PayingrollService;
-
+create table EmpPayroll(
+EmployeeId int identity(1,1) primary key,
+EmployeeName varchar(255),
+Salary float,
+StartingDate Date
+);
